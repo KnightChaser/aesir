4efe7438b99e79cadabdb73d2ac7a1744a065638
Lee Garam <agerio100@naver.com>
2024-01-27 01:28:32 +0900

    Improve MUX routing and HTML files
    Using subrouters, and adjust redirection URLs

--- main.go
+++ main.go
@@ -9,17 +9,24 @@ import (
 )
 
 func main() {
-
 	// Setting up web server with gorilla/mux
 	muxRouter := mux.NewRouter()
 
-	muxRouter.PathPrefix("/web/").Handler(http.StripPrefix("/web/", http.FileServer(http.Dir("./web"))))
-	muxRouter.PathPrefix("/web/entrance/").Handler(http.StripPrefix("/web/entrance/", http.FileServer(http.Dir("/web/entrance/"))))
-	muxRouter.PathPrefix("/web/asset/font/").Handler(http.StripPrefix("/web/asset/font/", http.FileServer(http.Dir("/web/asset/font/"))))
+	// Serve static files
+	staticSubrouter := muxRouter.PathPrefix("/web/").Subrouter()
+	staticSubrouter.PathPrefix("/").Handler(http.StripPrefix("/web/", http.FileServer(http.Dir("./web"))))
 
-	// Define a route for the root path "/"
+	// Serve /web static files
+	staticEntranceSubrouter := muxRouter.PathPrefix("/web/entrance/").Subrouter()
+	staticEntranceSubrouter.PathPrefix("/").Handler(http.StripPrefix("/web/entrance/", http.FileServer(http.Dir("/web/entrance/"))))
+
+	// Serve font files
+	assetSubrouter := muxRouter.PathPrefix("/web/asset/").Subrouter()
+	assetSubrouter.PathPrefix("/font/").Handler(http.StripPrefix("/web/asset/font/", http.FileServer(http.Dir("/web/asset/font/"))))
+
+	// Define main routes
 	muxRouter.HandleFunc("/", webRequestHandler.HomeHandler)
-	muxRouter.HandleFunc("/entrance/", webRequestHandler.InspectEVTXHandler)
+	muxRouter.HandleFunc("/entrance", webRequestHandler.InspectEVTXHandler)
 	muxRouter.HandleFunc("/entrance/upload", webRequestHandler.InspectEVTXUploadHandler)
 
 	// Start the server on port 8080
