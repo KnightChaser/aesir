da2cdcaba1165b77f02fc8a5e01eab51e1e01c90
Lee Garam <agerio100@naver.com>
2024-02-02 19:39:33 +0900

    Add selectize.js components and search button
    Button click event will gather the event

--- web/search/selectize.js
+++ web/search/selectize.js
@@ -135,7 +135,7 @@ function selectizeTabTargetImages() {
                     return (
                         "<div>" +
                         '<span class="label"><b>' +
-                        escape(item.targetImagesAbbreviateds) +
+                        escape(item.targetImagesAbbreviated) +
                         "</b><br>" +
                         "</span>" +
                         '<span class="caption">' + escape(item.count) + " matches" + "<br>" +
@@ -163,3 +163,89 @@ function selectizeTabTargetImages() {
 }
 
 selectizeTabTargetImages();
+
+function selectizeTabRuleName() {
+    searchConditionRuleName = [
+        {
+            $match: {
+                "event.eventdata.RuleName": { $ne: null }
+            }
+        },
+        {
+            $group: {
+                _id: "$event.eventdata.RuleName",
+                count: { $sum: 1 }
+            }
+        },
+        {
+            $sort: { count: -1 }
+        },
+        {
+            $project: {
+                _id: 0,
+                ruleName: "$_id",
+                count: 1
+            }
+        }
+    ];
+
+    fetchEventData(currentCollection, "aggregate", JSON.stringify(searchConditionRuleName)).then((data) => {
+        let targetImageStatistics = JSON.parse(data);
+
+        $("#rule-name-selectize-tab").selectize({
+            persist: false,
+            maxItems: null,
+            valueField: "ruleName",
+            labelField: "ruleName",
+            searchField: "ruleName",
+            options: targetImageStatistics,
+            render: {
+                item: function (item, escape) {
+                    return (
+                        "<div>" +
+                            "<span>" + escape(item.ruleName) + "</span>" +
+                            " <span>(" + escape(item.count) + " matches)</span>" +
+                        "</div>"
+                    );
+                },
+                option: function (item, escape) {
+                    return (
+                        "<div>" +
+                        '<span class="label"><b>' +
+                        escape(item.ruleName) +
+                        "</b><br>" +
+                        "</span>" +
+                        '<span class="caption">' + escape(item.count) + " matches" + "<br>" +
+                        "</span>" +
+                        "</div>"
+                    );
+                },
+            },
+            createFilter: function (input) {
+                var match, regex;
+
+                regex = new RegExp("^" + input + "$", "i");
+                match = sourceImagesAbbreviated.find(item => item.match(regex));
+                if (match) return false;
+
+                return true;
+            },
+            create: function (input) {
+                return { sourceImage: input };
+            },
+        });
+
+    });
+}
+
+selectizeTabRuleName();
+
+// If button (id = "search-form-submit-button") is clicked, print the selected values of the selectize.js dropdowns
+// Also, get the values of date range picker
+$("#search-form-submit-button").click(function () {
+    console.log("Source Image: " + $("#source-image-selectize-tab").val());
+    console.log("Target Image: " + $("#target-image-selectize-tab").val());
+    console.log("Rule Name: " + $("#rule-name-selectize-tab").val());
+    console.log("Date Range (start): " + $("#starting-datetime").val());
+    console.log("Date Range (end): " + $("#ending-datetime").val());
+});
